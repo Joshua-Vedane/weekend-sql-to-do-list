60c32fe88a5feac99c978a708d5e50ad1d88b062
Joshua Vedane <jvedane@gmail.com>
2021-01-09 09:08:41 -0600

    get ajax req and router get operational

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,26 @@ $(document).ready(handleReady);
 
 function handleReady(){
   console.log('jQuery ');
+  refreshTodos();
+}
+
+// get todos from server
+function refreshTodos() {
+  $.ajax({
+    type: 'GET',
+    url: '/todo'
+  }).then(function(response){
+    console.log('in refreshTodos',response);
+    //send data to render function
+    renderTodos(response);
+  }).catch(function(error){
+    console.log('error in GET', error);
+  });
+}
+
+
+
+//render todo to DOM
+function renderTodos(todos){
+  console.log('in renderTodos',todos);
 }
--- server/routes/todo_router.js
+++ server/routes/todo_router.js
@@ -4,7 +4,10 @@ const pool = require('../modules/pool');
 
 
 //GET ROUTE
-
+router.get('/', (req, res) => {
+  console.log('GET req to /todo');
+  res.sendStatus(200);
+})
 
 //POST ROUTE
 
